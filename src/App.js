461d96673981e85c407db688a0cdc4e4353b2beb
pranjal_srivastava@digitelt226 <pranjal.srivastava>
2024-03-05 22:11:04 +0530

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Reservation from "./components/Reservation";
 import { ReservationProvider } from "./components/ReservationContext";
 import DashBoard from "./components/DashBoard.jsx";
@@ -11,7 +11,8 @@ export default function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route exact path="/BusBookingReactApp" element={<DashBoard/>} />
+          <Route path="/" element={<Navigate to="/BusBookingReactApp" />} />
+          <Route path="/BusBookingReactApp" element={<DashBoard/>} />
           <Route path="/BusBookingReactApp/reservation" element={<Reservation />} />
         </Routes>
       </BrowserRouter>
